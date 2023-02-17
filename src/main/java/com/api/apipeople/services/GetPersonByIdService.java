b5581dfaa616e0eb625e3b71946aea5bbcf6bca6
feliperodsdev <libertyisrequired@gmail.com>
2023-02-17 11:41:22 -0300

    feat: getById service and test

--- src/main/java/com/api/apipeople/services/GetPersonByIdService.java
+++ src/main/java/com/api/apipeople/services/GetPersonByIdService.java
@@ -1,2 +1,20 @@
-package com.api.apipeople.services;public class GetPersonByIdService {
+package com.api.apipeople.services;
+
+import com.api.apipeople.entities.Person;
+import com.api.apipeople.repositories.IPersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetPersonByIdService {
+
+    private IPersonRepository personRepository;
+
+    public GetPersonByIdService(IPersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public Person execute(Long id){
+        return this.personRepository.getPersonById(id);
+    }
+
 }
--- src/test/java/com/api/apipeople/services/GetPersonByIdTest.java
+++ src/test/java/com/api/apipeople/services/GetPersonByIdTest.java
@@ -1,2 +1,34 @@
-package com.api.apipeople.services;public class GetPersonByIdTest {
+package com.api.apipeople.services;
+
+import com.api.apipeople.entities.Person;
+import com.api.apipeople.inMemoryDbPerson.PersonRepositoryinMemory;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.List;
+
+public class GetPersonByIdTest {
+
+
+
+    @Test
+    public void should_return_an_person() {
+        PersonRepositoryinMemory personRepositoryinMemory = new PersonRepositoryinMemory();
+        GetPersonByIdService getPersonByIdService = new GetPersonByIdService(personRepositoryinMemory);
+        ListPersonsService listPersonsService = new ListPersonsService(personRepositoryinMemory);
+
+        Person p1 = new Person(null, "Felipe", LocalDate.parse("2002-10-19"));
+        Person p2 = new Person(null, "Jordana", LocalDate.parse("2003-01-14"));
+
+        personRepositoryinMemory.save(p1);
+        personRepositoryinMemory.save(p2);
+
+        List<Person> person = listPersonsService.listPersons();
+
+        assertNotNull(getPersonByIdService.execute(person.get(0).getId()));
+        assertNotNull(getPersonByIdService.execute(person.get(1).getId()));
+        assertEquals(person.get(0).getName(), "Felipe");
+        assertEquals(person.get(1).getName(), "Jordana");
+    }
+
 }
