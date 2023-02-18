eefe4751a8498bd19b122af93b2d3c5921c7cd20
liber <libertyisrequired@gmail.com>
2023-02-18 00:51:52 -0300

    fix: fixing test

--- src/test/java/com/api/apipeople/inMemoryDbPerson/PersonRepositoryinMemory.java
+++ src/test/java/com/api/apipeople/inMemoryDbPerson/PersonRepositoryinMemory.java
@@ -2,6 +2,8 @@ package com.api.apipeople.inMemoryDbPerson;
 
 import com.api.apipeople.entities.Person;
 import com.api.apipeople.repositories.IPersonRepository;
+import com.api.apipeople.services.exceptions.ResourceNotFound;
+import net.bytebuddy.dynamic.DynamicType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,16 +25,13 @@ public class PersonRepositoryinMemory implements IPersonRepository {
         return persons;
     }
 
-    public Person getPersonById(Long id){
-        Person person = new Person();
-        for(Person x: persons){
-            if(x.getId() == id){
-                person.setId(x.getId());
-                person.setName(x.getName());
-                person.setDateOfBirth(x.getDateOfBirth());
+    public Optional<Person> getPersonById(Long id) {
+        for (Person person : persons) {
+            if (person.getId().equals(id)) {
+                return Optional.of(person);
             }
         }
-        return person;
+        return Optional.empty();
     }
 
 }
--- src/test/java/com/api/apipeople/services/GetPersonByIdTest.java
+++ src/test/java/com/api/apipeople/services/GetPersonByIdTest.java
@@ -2,14 +2,19 @@ package com.api.apipeople.services;
 
 import com.api.apipeople.entities.Person;
 import com.api.apipeople.inMemoryDbPerson.PersonRepositoryinMemory;
+import com.api.apipeople.services.exceptions.ResourceNotFound;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.util.List;
 
 public class GetPersonByIdTest {
 
-
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void should_return_an_person() {
@@ -29,6 +34,28 @@ public class GetPersonByIdTest {
         assertNotNull(getPersonByIdService.execute(person.get(1).getId()));
         assertEquals(person.get(0).getName(), "Felipe");
         assertEquals(person.get(1).getName(), "Jordana");
+    }
+
+    @Test
+    public void should_throw_resource_exception() {
+
+        exception.expect(ResourceNotFound.class);
+
+
+        PersonRepositoryinMemory personRepositoryinMemory = new PersonRepositoryinMemory();
+        GetPersonByIdService getPersonByIdService = new GetPersonByIdService(personRepositoryinMemory);
+        ListPersonsService listPersonsService = new ListPersonsService(personRepositoryinMemory);
+
+        Person p1 = new Person(null, "Felipe", LocalDate.parse("2002-10-19"));
+        Person p2 = new Person(null, "Jordana", LocalDate.parse("2003-01-14"));
+
+        personRepositoryinMemory.save(p1);
+        personRepositoryinMemory.save(p2);
+
+        List<Person> person = listPersonsService.listPersons();
+
+        getPersonByIdService.execute(person.get(0).getId() + person.get(1).getId());
+
     }
 
 }
