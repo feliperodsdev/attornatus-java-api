5c46c59c6220ba0d0496a1d1cd2bec3d9d5d05ae
liber <libertyisrequired@gmail.com>
2023-02-18 22:09:50 -0300

    feat: dto to create and address

---
+++ src/main/java/com/api/apipeople/dtos/CreateAddressDto.java
@@ -0,0 +1,41 @@
+package com.api.apipeople.dtos;
+
+public class CreateAddressDto {
+
+    private String streetAddress;
+    private String zipCode;
+    private String number;
+    private String city;
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
